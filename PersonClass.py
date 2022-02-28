be7ddd4e0fa5c8f57e1caa69964d358660e2aafe
James Allen <james_allen2@baylor.edu>
2022-02-28 15:44:15 -0600

    class notes 2/28

---
+++ PersonClass.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, name, address, telephone_number):
+        self.__name = name
+        self.__address = address
+        self.__telephone_number = telephone_number
+
+    def print_person(self):
+        print("The person's name is: ", self.__name)
+        print("The person's address is: ", self.__address)
+        print("The person's telephone number is: ", self.__telephone_number)
+
+
+class Customer(Person):
+    def __init__(self, name, address, telephone_number, customer_number, mailing_list):
+
+        Person.__init__(self, name, address, telephone_number)
+
+        self.__customer_number = customer_number
+        self.__mailing_list = mailing_list
+
+        self.print_person()
+
+    def print_person(self):
+        print("The customer's customer_number is: ", self.__customer_number)
+        print("The customer's mailing list preference is: ", self.__mailing_list)
---
+++ PersonProgram.py
@@ -0,0 +1,12 @@
+import PersonClass as pc
+
+per = pc.Person(
+    "James",
+    "1025 La Salle Ave",
+    8172338016,
+)
+
+cust = pc.Customer("Andrew", "1025 La Salle", 8171234567, "001", True)
+
+per.print_peron()
+cust.print_person()
